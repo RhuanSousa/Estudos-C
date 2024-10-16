ec019a90f55cb8e07519a5030626da16a7e6a48c
Rhuan <Rhuansantos.cp@gmail.com>
2024-10-16 18:24:58 -0300

    estudando strings

--- Exercicios Cap5/ex010/main.c
+++ Exercicios Cap5/ex010/main.c
@@ -3,8 +3,22 @@
 
 int main(void) {
 
+    char s[19];
 
-    
+    printf("Digite algo (leitura pelo gets): \n");
+    gets(s);
+    fflush(stdin);
+
+    puts("Resultado: ");
+    puts(s);
+    puts("");
+
+    printf("Digite algo (leitura pelo fgets): \n");
+    fgets(s, 19, stdin);
+    fflush(stdin);
+
+    puts("Resultado: ");
+    puts(s);
 
     system("pause");
     return 0;
