86fd282cc7c647d983dca0486cf368bfb78f7847
Rhuan <Rhuansantos.cp@gmail.com>
2024-10-28 17:56:43 -0300

    atividades

--- Exercicios Cap5/ex018/main.c
+++ Exercicios Cap5/ex018/main.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
+#include <windows.h>
 
-int main(void) {
+int main(void)
+{
 
-    printf("");
+    int num, i;
+
+    system("cls");
+    do
+    {
+        printf("\nDigite um numero maior do que 1: ");
+        scanf("%d", &num);
+
+        if (num <= 1)
+        {
+            printf("Numero invalido!");
+        }
+    } while (num <= 1);
+
+    for (i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            printf("numero nao e primo");
+            break;
+        }
+    }
+    if (i == num && num % i == 0)
+    {
+        printf("O numero e primo");
+    }
+
     return 0;
 }
--- Exercicios Cap5/ex019/main.c
+++ Exercicios Cap5/ex019/main.c
@@ -2,5 +2,17 @@
 
 int main(void) {
 
+    float s = 0, numerador = 1, denominador = 1;
+
+    while (numerador <= 99 && denominador <= 55)
+    {
+        s += numerador/denominador;
+
+        numerador +=2;
+        denominador++;
+    }
+
+    printf("%.2f", s);
+    
     return 0;
 }
