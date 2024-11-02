ca5243ee7d81ec654f3540c598621449e1f925f7
Rhuan <Rhuansantos.cp@gmail.com>
2024-11-02 10:12:37 -0300

    atividade

--- Exericicios Cap6/Vetores/ex001/main.c
+++ Exericicios Cap6/Vetores/ex001/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
 
+    int vetor[6];
+    int i;
+
+    for (i = 0; i < 6; i++)
+    {
+        printf("Digite o %d valor: ", i+1);
+        scanf("%d", &vetor[i]);
+    }
+
+    for (i = 0; i < 6; i++)
+    {
+        printf("\n%d", vetor[i]);
+    }
     return 0;
 }
