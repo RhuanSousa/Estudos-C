a07b02e3a84feebfb07b64d95249adf48e66e72a
Rhuan <Rhuansantos.cp@gmail.com>
2024-11-22 17:32:31 -0300

    uybfewf

--- Exercicios Cap6/Vetores/ex014/main.c
+++ Exercicios Cap6/Vetores/ex014/main.c
@@ -1,6 +1,23 @@
+// Faça um programa que calcule o desvio-padrão d de um vetor V contendo n números
+
 #include <stdio.h>
+#define n 10
+#define tam 5
 
 int main(void) {
+    int vetor[tam];
+    float media[tam];
+
+    soma += vetor[i];
+
+    media[i] = soma/tam;
+
+
+    for (size_t i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
 
     return 0;
 }
